221718f775a1b6207f093dbce563e48564e2debc
Jamie Norrish <jamie@artefact.org.nz>
2014-07-01 10:53:08 +0900

    Added admin for LogEntry. Added searching and filtering to some model admins.

--- attribution/admin.py
+++ attribution/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.core.urlresolvers import NoReverseMatch, reverse
+from django.utils.html import escape
 
 from .models import Date, Identifier, Person, PropertyAssertion, Source, Text, Title
 
@@ -20,19 +22,54 @@ class IdentifierInline (admin.TabularInline):
     model = Identifier
 
 
-class TextAssertionsInline (admin.TabularInline):
-
-    model = PropertyAssertion.texts.through
-
-
 class TitleInline (admin.TabularInline):
 
     extra = 1
     model = Title
 
 
+class LogEntryAdmin (admin.ModelAdmin):
+
+    # Code from https://djangosnippets.org/snippets/3009/
+
+    date_hierarchy = 'action_time'
+    readonly_fields = admin.models.LogEntry._meta.get_all_field_names()
+    list_filter = ['user', 'content_type', 'action_flag']
+    search_fields = ['object_repr', 'change_message']
+    list_display = ['action_time', 'user', 'content_type', 'object_link',
+                    'action_flag', 'change_message']
+
+    def has_add_permission (self, request):
+        return False
+
+    def has_delete_permission (self, request, obj=None):
+        return False
+
+    def object_link (self, obj):
+        ct = obj.content_type
+        repr_ = escape(obj.object_repr)
+        try:
+            href = reverse('admin:%s_%s_change' % (ct.app_label, ct.model),
+                        args=[obj.object_id])
+            link = u'<a href="%s">%s</a>' % (href, repr_)
+        except NoReverseMatch:
+            link = repr_
+        return link if obj.action_flag != admin.models.DELETION else repr_
+    object_link.allow_tags = True
+    object_link.admin_order_field = 'object_repr'
+    object_link.short_description = u'object'
+
+    def queryset (self, request):
+        return super(LogEntryAdmin, self).queryset(request) \
+            .prefetch_related('content_type')
+
+
+
 class PropertyAssertionAdmin (admin.ModelAdmin):
 
+    list_display = ('source', 'argument', 'is_preferred')
+    list_filter = ('is_preferred',)
+    search_fields = ('source', 'argument')
     fieldsets = (
         (None, {'fields': ('texts',)}),
         ('People', {'fields': ('authors', 'translators')}),
@@ -52,12 +89,14 @@ class PropertyAssertionAdmin (admin.ModelAdmin):
 
 class SourceAdmin (NamableAdmin, admin.ModelAdmin):
 
-    pass
+    list_display = ('name', 'date', 'abbreviation')
+    search_fields = ['name']
 
 
 class TextAdmin (admin.ModelAdmin):
 
     actions = ['regenerate_identifier']
+    search_fields = ('cached_identifier__identifier',)
 
     def regenerate_identifier (self, request, queryset):
         for text in queryset:
@@ -66,6 +105,7 @@ class TextAdmin (admin.ModelAdmin):
 
 
 admin.site.register(Date)
+admin.site.register(admin.models.LogEntry, LogEntryAdmin)
 admin.site.register(Person)
 admin.site.register(PropertyAssertion, PropertyAssertionAdmin)
 admin.site.register(Source, SourceAdmin)
