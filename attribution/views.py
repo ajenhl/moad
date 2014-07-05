6bfaeb8e2a39e2601e1044e3d7109ec9884a9be4
Jamie Norrish <jamie@artefact.org.nz>
2014-07-05 15:35:10 +0100

    Added pagination and filtering of display lists for people, sources, and texts.

--- attribution/views.py
+++ attribution/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
 
 import attribution.models
@@ -13,8 +14,23 @@ def person_display (request, person_id):
     return render(request, 'attribution/display/person.html', context)
 
 def person_list_display (request):
-    people = attribution.models.Person.objects.all()
-    context = {'people': people}
+    person_list = attribution.models.Person.objects.all()
+    filter_text = request.GET.get('q', '')
+    if filter_text:
+        person_list = person_list.filter(name__icontains=filter_text)
+    paginator = Paginator(person_list, 50)
+    page = request.GET.get('page')
+    query_parameters = request.GET.copy()
+    query_parameters.pop('page', None)
+    try:
+        people = paginator.page(page)
+    except PageNotAnInteger:
+        people = paginator.page(1)
+    except EmptyPage:
+        people = paginator.page(paginator.num_pages)
+    context = {'current_page': people.number, 'filter': filter_text,
+               'num_pages': paginator.num_pages, 'people': people,
+               'query_parameters': query_parameters}
     return render(request, 'attribution/display/person_list.html', context)
 
 def source_display (request, source_id):
@@ -23,8 +39,23 @@ def source_display (request, source_id):
     return render(request, 'attribution/display/source.html', context)
 
 def source_list_display (request):
-    sources = attribution.models.Source.objects.all()
-    context = {'sources': sources}
+    source_list = attribution.models.Source.objects.all()
+    filter_text = request.GET.get('q', '')
+    if filter_text:
+        source_list = source_list.filter(name__icontains=filter_text)
+    paginator = Paginator(source_list, 50)
+    page = request.GET.get('page')
+    query_parameters = request.GET.copy()
+    query_parameters.pop('page', None)
+    try:
+        sources = paginator.page(page)
+    except PageNotAnInteger:
+        sources = paginator.page(1)
+    except EmptyPage:
+        sources = paginator.page(paginator.num_pages)
+    context = {'current_page': sources.number, 'filter': filter_text,
+               'num_pages': paginator.num_pages, 'sources': sources,
+               'query_parameters': query_parameters}
     return render(request, 'attribution/display/source_list.html', context)
 
 def text_display (request, text_id):
@@ -35,7 +66,22 @@ def text_display (request, text_id):
     return render(request, 'attribution/display/text.html', context)
 
 def text_list_display (request):
-    texts = attribution.models.Text.objects.select_related('cached_identifier').order_by(
-        'cached_identifier__identifier')
-    context = {'texts': texts}
+    text_list = attribution.models.Text.objects.select_related('cached_identifier').order_by('cached_identifier__identifier')
+    filter_text = request.GET.get('q', '')
+    if filter_text:
+        text_list = text_list.filter(
+            cached_identifier__identifier__icontains=filter_text)
+    paginator = Paginator(text_list, 50)
+    page = request.GET.get('page')
+    query_parameters = request.GET.copy()
+    query_parameters.pop('page', None)
+    try:
+        texts = paginator.page(page)
+    except PageNotAnInteger:
+        texts = paginator.page(1)
+    except EmptyPage:
+        texts = paginator.page(paginator.num_pages)
+    context = {'current_page': texts.number, 'filter': filter_text,
+               'num_pages': paginator.num_pages, 'texts': texts,
+               'query_parameters': query_parameters}
     return render(request, 'attribution/display/text_list.html', context)
