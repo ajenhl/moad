9d088d57d60834a84d67738f2e47495be3fb9978
Jamie Norrish <jamie@artefact.org.nz>
2015-03-17 19:16:36 +1300

    Added hackish support for the searching within DataTables to match on unaccented search terms.

--- attribution/templatetags/search_index.py
+++ attribution/templatetags/search_index.py
@@ -8,5 +8,7 @@ register = template.Library()
 
 @register.filter
 def remove_combining (text):
+    if type(text) != type(u''):
+        text = text.decode('utf-8')
     nkfd_form = unicodedata.normalize('NFKD', text)
     return u''.join([c for c in nkfd_form if not unicodedata.combining(c)])
