66f0230ec5cb715eb87bb189a39f2c91c689ecd4
Jamie Norrish <jamie@artefact.org.nz>
2015-11-02 14:49:17 +1300

    Add split forms of Txxxx etc identifiers to the search index so that people can do searches in the form they're accustomed to using.

--- attribution/templatetags/search_index.py
+++ attribution/templatetags/search_index.py
@@ -1,3 +1,4 @@
+import re
 import unicodedata
 
 from django import template
@@ -12,3 +13,12 @@ def remove_combining (text):
         text = text.decode('utf-8')
     nkfd_form = unicodedata.normalize('NFKD', text)
     return u''.join([c for c in nkfd_form if not unicodedata.combining(c)])
+
+@register.filter
+def split_identifier (text):
+    if type(text) != type(u''):
+        text = text.decode('utf-8')
+    match = re.search(r'^(JB|T|X|ZW)([0-9]+)$', text)
+    if match is not None:
+        text = '{} {} {}'.format(text, match.group(1), match.group(2))
+    return text
