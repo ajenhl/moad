90623a2d3b9fb9759ad57873475e354ed4372a58
Jamie Norrish <jamie@artefact.org.nz>
2018-04-23 20:21:06 +1200

    Removed unnecessary decoding of input that is never bytes.

--- attribution/templatetags/search_index.py
+++ attribution/templatetags/search_index.py
@@ -8,16 +8,13 @@ register = template.Library()
 
 
 @register.filter
-def remove_combining (text):
-    if type(text) != type(''):
-        text = text.decode('utf-8')
+def remove_combining(text):
     nkfd_form = unicodedata.normalize('NFKD', text)
-    return u''.join([c for c in nkfd_form if not unicodedata.combining(c)])
+    return ''.join([c for c in nkfd_form if not unicodedata.combining(c)])
+
 
 @register.filter
-def split_identifier (text):
-    if type(text) != type(''):
-        text = text.decode('utf-8')
+def split_identifier(text):
     match = re.search(r'^(JB|T|X|ZW)([0-9]+)$', text)
     if match is not None:
         number_zeros = 4 - len(match.group(2))
